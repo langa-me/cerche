63c4b3f320673eeffa3304e4c86e0c23f23a9436
louis030195 <louis.beaumont@gmail.com>
2022-04-04 03:13:04 +0200

    0.1.0: first release

--- setup.py
+++ setup.py
@@ -1,1 +1,27 @@
-version = "0.1.0"
+from setuptools import setup, find_packages
+from pathlib import Path
+
+if __name__ == "__main__":
+    with Path(Path(__file__).parent, "README.md").open(encoding="utf-8") as file:
+        long_description = file.read()
+
+    setup(
+        name="search-engine",
+        packages=find_packages(),
+        include_package_data=True,
+        version="0.1.0",
+        description="",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        data_files=[(".", ["README.md"])],
+        install_requires=[
+            # TODO
+        ],
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python :: 3.6",
+        ],
+    )
